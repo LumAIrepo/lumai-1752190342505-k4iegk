442aaaa030691c678dad67b01effd9cc4de747f6
LumAI <zurichixbt@gmail.com>
2025-07-11 01:32:32 +0200

    Add next.config.js

---
+++ next.config.js
@@ -0,0 +1,28 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
+      },
+    },
+  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+    };
+    return config;
+  },
+  transpilePackages: ['@solana/web3.js'],
+  images: {
+    domains: ['localhost'],
+  },
+};
+
+module.exports = nextConfig;
